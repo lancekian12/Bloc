6384b7f26b08fb598de6d5fc9d83017edcf667c1
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-09-10 02:31:03 +0800

    Created folders and created home screen

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:bloc_counter/logic/cubit/counter_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:bloc_counter/presentation/screen/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider<CounterCubit>(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        title: 'Flutter Concept',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: const HomeScreen(
+          title: "Flutter",
+          color: Colors.blueAccent,
+        ),
+      ),
+    );
+  }
+}
